bff878342f03d9ce4f3896d6bd8dfeb534759149
aeroajith <nexusrobotech2022@email.com>
2023-07-27 13:12:47 +0530

    Fourth Commit

--- myproject/settings.py
+++ myproject/settings.py
@@ -94,7 +94,6 @@ WSGI_APPLICATION = 'myproject.wsgi.application'
 CORS_ALLOW_ALL_ORIGINS: False
 
 CORS_ALLOWED_ORIGINS = [
-    "http://localhost:3000",
     "http://127.0.0.1:8000",
     "http://localhost:8000",
     "http://44.202.6.197"
