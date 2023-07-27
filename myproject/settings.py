908fe455f90aac132f45a30ad781be3adcf91720
aeroajith <nexusrobotech2022@email.com>
2023-07-27 14:06:32 +0530

    ssix

--- myproject/settings.py
+++ myproject/settings.py
@@ -92,12 +92,11 @@ SIMPLE_JWT = {
 
 WSGI_APPLICATION = 'myproject.wsgi.application'
 
-CORS_ALLOW_ALL_ORIGINS: True
+CORS_ALLOW_ALL_ORIGINS: False
 
 CORS_ALLOWED_ORIGINS = [
-    "http://localhost:8000",
     "http://127.0.0.1:8000",
-    "http://44.202.6.197/"
+    "http://44.202.6.197"
 ]
 
 
